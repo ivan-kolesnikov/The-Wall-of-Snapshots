b28e3ca16fb2080f705fcabe8904058e91c003f5
ivan-kolesnikov <ivan.kolesnikov.fkn@gmail.com>
2018-10-03 17:47:54 +0300

    updated serializers, 80% completed

--- twos_rest/twos_rest/twos_rest_api/serializers.py
+++ twos_rest/twos_rest/twos_rest_api/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Channel, Event, CC_error, UDP_error, Updown_error
+from django.db.models import Q
 from django.utils.datastructures import MultiValueDictKeyError
 from rest_framework.validators import UniqueTogetherValidator
 
@@ -93,7 +94,12 @@ class CcUdpUpDownEventSerializer(serializers.ModelSerializer):
 
 # Complex Events Channels
 class CcEventChannelSerializer(serializers.ModelSerializer):
-    events = CcEventSerializer(many=True)
+    events = serializers.SerializerMethodField('get_cc_events')
+
+    def get_cc_events(self, channel):
+        filtered_events = Event.objects.filter(CC_errors__isnull=False, channel=channel)
+        serializer = CcEventSerializer(instance=filtered_events, many=True)
+        return serializer.data
 
     class Meta:
         model = Channel
@@ -101,7 +107,12 @@ class CcEventChannelSerializer(serializers.ModelSerializer):
 
 
 class UdpEventChannelSerializer(serializers.ModelSerializer):
-    events = UdpEventSerializer(many=True)
+    events = serializers.SerializerMethodField('get_udp_events')
+
+    def get_udp_events(self, channel):
+        filtered_events = Event.objects.filter(UDP_errors__isnull=False, channel=channel)
+        serializer = UdpEventSerializer(instance=filtered_events, many=True)
+        return serializer.data
 
     class Meta:
         model = Channel
@@ -109,7 +120,12 @@ class UdpEventChannelSerializer(serializers.ModelSerializer):
 
 
 class UpDownEventChannelSerializer(serializers.ModelSerializer):
-    events = UpDownEventSerializer(many=True)
+    events = serializers.SerializerMethodField('get_updown_events')
+
+    def get_updown_events(self, channel):
+        filtered_events = Event.objects.filter(Updown_errors__isnull=False, channel=channel)
+        serializer = UpDownEventSerializer(instance=filtered_events, many=True)
+        return serializer.data
 
     class Meta:
         model = Channel
@@ -117,7 +133,15 @@ class UpDownEventChannelSerializer(serializers.ModelSerializer):
 
 
 class CcUdpEventChannelSerializer(serializers.ModelSerializer):
-    events = CcUdpEventSerializer(many=True)
+    events = serializers.SerializerMethodField('get_cc_udp_events')
+
+    def get_cc_udp_events(self, channel):
+        exist_cc_error = Q(CC_errors__isnull=False)
+        exist_udp_error = Q(UDP_errors__isnull=False)
+        # CC or UDP errors should be in the events list to add them in the response
+        filtered_events = Event.objects.filter(exist_cc_error or exist_udp_error, channel=channel)
+        serializer = CcUdpEventSerializer(instance=filtered_events, many=True)
+        return serializer.data
 
     class Meta:
         model = Channel
@@ -125,7 +149,15 @@ class CcUdpEventChannelSerializer(serializers.ModelSerializer):
 
 
 class CcUpDownEventChannelSerializer(serializers.ModelSerializer):
-    events = CcUpDownEventSerializer(many=True)
+    events = serializers.SerializerMethodField('get_cc_updown_events')
+
+    def get_cc_updown_events(self, channel):
+        exist_cc_error = Q(CC_errors__isnull=False)
+        exist_updown_event = Q(Updown_errors__isnull=False)
+        # CC error or Updown event should be in the events list to add them in the response
+        filtered_events = Event.objects.filter(exist_cc_error or exist_updown_event, channel=channel)
+        serializer = CcUpDownEventSerializer(instance=filtered_events, many=True)
+        return serializer.data
 
     class Meta:
         model = Channel
@@ -133,7 +165,15 @@ class CcUpDownEventChannelSerializer(serializers.ModelSerializer):
 
 
 class UdpUpDownEventChannelSerializer(serializers.ModelSerializer):
-    events = UdpUpDownEventSerializer(many=True)
+    events = serializers.SerializerMethodField('get_udp_updown_events')
+
+    def get_udp_updown_events(self, channel):
+        exist_udp_error = Q(UDP_errors__isnull=False)
+        exist_updown_event = Q(Updown_errors__isnull=False)
+        # UDP error or Updown event should be in the events list to add them in the response
+        filtered_events = Event.objects.filter(exist_udp_error or exist_updown_event, channel=channel)
+        serializer = UdpUpDownEventSerializer(instance=filtered_events, many=True)
+        return serializer.data
 
     class Meta:
         model = Channel
